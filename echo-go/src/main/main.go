ee6fccec9e8b21f319c3c67e46d8079baed90e0f
felipematthew <felipematthewnascimento@gmail.com>
2024-07-03 21:25:08 -0300

    ✨ feat: Basic Auth

--- echo-go/src/main/main.go
+++ echo-go/src/main/main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/FelipeMatthew/go-learnings/src/functions"
 	"github.com/FelipeMatthew/go-learnings/src/middlewares"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 )
-
-
 
 func main() {
 	fmt.Println("Working w echo")
@@ -24,14 +23,20 @@ func main() {
 	e.POST("/moto", functions.AddMoto)
 	e.POST("/dog", functions.AddDog)
 
-
 	// Middlewares - Groups
 	g := e.Group("/admin")
 
 	// Can use
-	g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
-		Format: `[${time_rfc339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
-	}))
+	// g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+	// 	Format: `[${time_rfc339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
+	// }))
+
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "felpola" && password == "123" {
+				return true, nil
+		}
+		return false, nil
+}))
 
 	g.GET("/main", middlewares.MainAdmin)
 
