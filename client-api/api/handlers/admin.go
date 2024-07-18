d472cb4c476bfc89b3b7b84ac370c6f09f2fb8c5
felipe <felipematthewnascimento@gmail.com>
2024-07-18 10:51:11 -0300

    ✨ feat: Get admins

--- client-api/api/handlers/admin.go
+++ client-api/api/handlers/admin.go
@@ -3,11 +3,28 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/felipematthew/go-learnings/client-api/api/config"
+	"github.com/felipematthew/go-learnings/client-api/api/models"
 	"github.com/labstack/echo/v4"
 )
 
 func GetAdmin(c echo.Context) error {
-	return c.String(http.StatusOK, "getting admin account")
+	db := config.DB()
+
+	admins := []*models.Admin{}
+
+	if err := db.Find(&admins).Error; err != nil {
+		data := map[string]interface{}{
+			"message": err.Error(),
+		}
+		return c.JSON(http.StatusInternalServerError, data)
+	}
+
+	response := map[string]interface{}{
+		"data": admins, 
+	}
+
+	return c.JSON(http.StatusOK, response)
 }
 
 func CreateAdmin(c echo.Context) error {
