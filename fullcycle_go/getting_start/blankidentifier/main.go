78e49a1573c7b94192a55974c2fab976c665342e
felipe <felipematthewnascimento@gmail.com>
2024-07-23 11:39:06 -0300

    Blank identifier and crawler

---
+++ fullcycle_go/getting_start/blankidentifier/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+
+	res, _ := http.Get("http://google.com")
+	body, _ := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	fmt.Printf("%s", body)
+}
