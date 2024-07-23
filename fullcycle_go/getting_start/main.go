dc99003f2c49fac2c37823d27c3f51e580988967
felipe <felipematthewnascimento@gmail.com>
2024-07-23 10:38:51 -0300

    Fullcycle go couse

---
+++ fullcycle_go/getting_start/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10 // int
+	var b string = "hello world"
+	var c, d string = "hello", "world"
+	var e int
+	e = 3
+
+	fmt.Println(a, b, c, d, e)
+}
