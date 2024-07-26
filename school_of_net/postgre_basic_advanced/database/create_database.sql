437fb6618ce78fd7c1009d84b901f6028b62cdb4
felipe <felipematthewnascimento@gmail.com>
2024-07-26 11:33:22 -0300

    Create DB and empregado table

---
+++ school_of_net/postgre_basic_advanced/database/create_database.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE schoolofnet
+    WITH
+    OWNER = postgres
+    TEMPLATE = template0
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+COMMENT ON DATABASE schoolofnet
+    IS 'just to studing database';
---
+++ school_of_net/postgre_basic_advanced/notes.md
@@ -0,0 +1,9 @@
+Entidade - Relacionamentos
+
+SGBD - Relacional - Possui dados relacionados.
+
+Tuplas - Linhas
+
+ID
+
+Autovacuum - vai apagar as poeiras do banco de dados e vai organizar elas automaticamente
