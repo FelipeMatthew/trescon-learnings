822d0ab8e463ea418f0a1a73a8364a38e055de44
felipe <felipematthewnascimento@gmail.com>
2024-07-29 13:53:22 -0300

    Route initial config

---
+++ minIO/PDFStore/api/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"pdfstore/api/handlers"
+
+	"github.com/labstack/echo/v4"
+)
+
+func SetupRoutes(e *echo.Echo) {
+
+	minio := e.Group("/minio")
+
+	minio.GET("/", handlers.GetFiles)
+}
