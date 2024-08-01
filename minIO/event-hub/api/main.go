075dd562de8d7c0204164a70bccbd550656e625a
felipe <felipematthewnascimento@gmail.com>
2024-08-01 11:14:03 -0300

    MinIO Config

--- minIO/event-hub/api/main.go
+++ minIO/event-hub/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/felipematthew/eventhub-api/api/config"
 	"github.com/felipematthew/eventhub-api/api/routes"
 	"github.com/labstack/echo/v4"
 )
@@ -11,6 +12,8 @@ func main() {
 	e := echo.New()
 
 	routes.StartRoutes(e)
+
+	config.DatabaseInit()
 
 	log.Fatal(e.Start(":8000"))
 }
