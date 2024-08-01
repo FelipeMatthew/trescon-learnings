e86327a9ea8dd2218fd480f74968b4b4d9eefa43
felipe <felipematthewnascimento@gmail.com>
2024-08-01 15:10:54 -0300

    Login user

--- minIO/event-hub/api/api/handlers/admin.go
+++ minIO/event-hub/api/api/handlers/admin.go
@@ -46,5 +46,25 @@ func CreateAdmin(c echo.Context) error {
 }
 
 func Login(c echo.Context) error {
+	db := config.DB()
 
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+
+	var count int64
+	// Query
+	query := "SELECT COUNT(*) FROM admins WHERE username = ? AND password = crypt(?, password)"
+	result := db.Raw(query, username, password).Count(&count)
+
+	// Vai pegar e se o contador achar o user e senha ele retorna 1
+	if result.Error != nil {
+		return c.JSON(http.StatusInternalServerError, echo.Map{"error": result.Error.Error()})
+	}
+
+	if count > 0 {
+		return c.JSON(http.StatusOK, echo.Map{"message": "Authentication successful"})
+		// TODO JWT
+	}
+
+	return c.JSON(http.StatusUnauthorized, echo.Map{"message": "Invalid credentials"})
 }
