9a36e8a716b76765abc83740bb2b6366064c01cb
felipe <felipematthewnascimento@gmail.com>
2024-08-01 13:08:44 -0300

    Ping handler done

--- minIO/event-hub/api/api/handlers/ping.go
+++ minIO/event-hub/api/api/handlers/ping.go
@@ -3,9 +3,46 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/felipematthew/eventhub-api/api/utils"
 	"github.com/labstack/echo/v4"
 )
 
 func Ping(c echo.Context) error {
-	return c.String(http.StatusOK, "get events")
+	// Minio variables
+	minioConn := utils.CheckMinioConnection()
+	minioEndpoint := "localhost"
+	minioUser := "admin"
+	minioPort := "9000"
+
+	// Db variables
+	dbConn := utils.CheckDbConnection()
+	dbEndpoint := "localhost"
+	dbUser := "postgres"
+	dbName := "eventhub"
+	dbPort := "5432"
+
+	// version and build
+	version := "2.3"
+	build := "3.0"
+
+	// Output
+	output := echo.Map{
+		"minio_config": map[string]interface{}{
+			"connected": minioConn,
+			"endpoint":  minioEndpoint,
+			"user":      minioUser,
+			"port":      minioPort,
+		},
+		"db_config": map[string]interface{}{
+			"connected": dbConn,
+			"name":      dbName,
+			"endpoint":  dbEndpoint,
+			"user":      dbUser,
+			"port":      dbPort,
+		},
+		"vesion": version,
+		"build":  build,
+	}
+
+	return c.JSON(http.StatusOK, output)
 }
