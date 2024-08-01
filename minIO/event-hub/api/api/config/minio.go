2be5400479537a4d665b5d31bcfb761e12d866bf
felipe <felipematthewnascimento@gmail.com>
2024-08-01 11:13:07 -0300

    MinIO config

--- minIO/event-hub/api/api/config/minio.go
+++ minIO/event-hub/api/api/config/minio.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"log"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
+)
+
+var MinioClient *minio.Client
+
+func MinioInit() {
+	var err error
+	endpoint := "localhost:9000"
+	accessKeyID := "admin"
+	secretAccessKey := "password"
+	useSSL := false
+
+	MinioClient, err = minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+
+	if err != nil {
+		log.Fatalf("Failed to initialize MinIO client: %v", err)
+	}
+}
