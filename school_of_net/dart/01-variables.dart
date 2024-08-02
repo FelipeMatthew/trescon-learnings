1f452f797abe7906e693cf08bea1d244edfe1be9
felipe <felipematthewnascimento@gmail.com>
2024-08-02 10:17:12 -0300

    Var and hello w/ dart

---
+++ school_of_net/dart/01-variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  // Atribuindo valores
+  // Fortemente tipado
+  String name = "Felipe";
+  name = "Matthew";
+  name = "New name";
+
+  int age = 20;
+  double salary = 20.50;
+  bool isItTrue = false;
+
+  print(name);
+  print(age);
+  print(salary);
+  print(isItTrue);
+}
