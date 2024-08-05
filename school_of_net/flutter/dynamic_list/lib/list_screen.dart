824850bb195329bf4826144aaf40c3912e17f645
felipe <felipematthewnascimento@gmail.com>
2024-08-05 15:48:08 -0300

    dynamic list - creating the list

---
+++ school_of_net/flutter/dynamic_list/lib/list_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatefulWidget {
+  const ListScreen({super.key});
+
+  @override
+  State<ListScreen> createState() => _ListScreenState();
+}
+
+class _ListScreenState extends State<ListScreen> {
+  List<String> todo = [
+    'To do 1',
+    'To do 2',
+    'To do 3',
+    'To do 4',
+    'To do 5',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dynamic list'),
+        backgroundColor: Colors.pinkAccent,
+        centerTitle: true,
+      ),
+      body: ListView.separated(
+        separatorBuilder: (context, index) => Divider(
+          color: Colors.pinkAccent,
+        ),
+        itemCount: todo.length,
+        itemBuilder: (BuildContext context, index) {
+          return ListTile(
+            onTap: () {},
+            leading: CircleAvatar(
+              child: IconTheme(
+              child: Icon(Icons.ac_unit),
+              data: IconThemeData(
+                color: Colors.white,
+              ), // Icon color
+            ),
+            backgroundColor: Colors.pinkAccent,
+            ),
+            title: Text(
+              todo[index],
+              style: TextStyle(color: Colors.pinkAccent),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ school_of_net/flutter/dynamic_list/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:dynamic_list/list_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dynamic list',
+      home: ListScreen(),
+    );
+  }
+}
