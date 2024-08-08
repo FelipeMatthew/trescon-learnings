a71be81aca86c134429d48f75eb2ceb41174b66c
felipematthew <felipematthewnascimento@gmail.com>
2024-08-08 20:51:22 -0300

    transferindo do main para home file

--- flutter/magalu_app/lib/pages/home.dart
+++ flutter/magalu_app/lib/pages/home.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+import 'package:magalu/pages/shopping_cart.dart';
+import 'package:magalu/widgets/app_bar_search.dart';
+import 'package:magalu/widgets/app_drawer_menu.dart';
+
+class HomeMyApp extends StatefulWidget {
+  const HomeMyApp({super.key});
 
   @override
-  State<HomePage> createState() => _HomePageState();
+  State<HomeMyApp> createState() => _HomeMyAppState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomeMyAppState extends State<HomeMyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        // * Menu drawer
+        drawer: const Drawer(
+          width: 250,
+          child: MenuDrawer(),
+        ),
+        appBar: AppBar(
+          title: const Text(
+            'Magazine Luiza',
+          ),
+          iconTheme: const IconThemeData(color: Colors.white),
+          titleTextStyle: const TextStyle(
+              color: Colors.white, fontSize: 20.0, fontWeight: FontWeight.bold),
+          backgroundColor: Colors.blue,
+          actions: <Widget>[
+            IconButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ShoppingCart()));
+                },
+                icon: const Icon(Icons.shopping_cart)),
+            IconButton(
+                onPressed: () {
+                  showSearch(context: context, delegate: AppSearchBar());
+                },
+                icon: const Icon(Icons.search)),
+          ],
+        ),
+        body: RefreshIndicator(
+          onRefresh: _refresh,
+          child: const Text('to'),
+        ));
+  }
+
+  Future<void> _refresh() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    setState(() {
+      Future.value();
+    });
+    return Future.value();
   }
 }
+
+
+
+// TODO Create scafold file 
+ 
+
--- flutter/magalu_app/lib/pages/splash.dart
+++ flutter/magalu_app/lib/pages/splash.dart
@@ -22,7 +22,7 @@ class _SplashState extends State<Splash> {
                   Container(
                     height: 50,
                   ),
-                  CircularProgressIndicator(
+                  const CircularProgressIndicator(
                     color: Colors.blue,
                   ),
                   Container(
