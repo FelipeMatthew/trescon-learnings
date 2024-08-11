c59a58a2fa355381e188ae7b6ec8c1b7c75d90f6
felipematthew <felipematthewnascimento@gmail.com>
2024-08-10 21:59:25 -0300

    Finished first page

--- flutter/github_finder/lib/pages/home.dart
+++ flutter/github_finder/lib/pages/home.dart
@@ -6,19 +6,72 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-          width: double.maxFinite,
-          decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius:
-                  BorderRadius.only(bottomLeft: Radius.circular(100))),
-          child: Padding(
-            padding: const EdgeInsets.all(70),
-            child: Image.asset(
-              'images/github-logo.png',
-              height: 200,
-            ),
-          )),
-    );
+        resizeToAvoidBottomInset:
+            false, // Vai evitar erro do input ficar por de trás do teclado
+        body: Column(
+          children: [
+            Container(
+                width: double.maxFinite,
+                decoration: const BoxDecoration(
+                    color: Colors.black,
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(100))),
+                child: Padding(
+                  padding: const EdgeInsets.all(70),
+                  child: Image.asset(
+                    'images/github-logo.png',
+                    height: 200,
+                  ),
+                )),
+            Padding(
+              padding: const EdgeInsets.all(40),
+              child: Column(children: [
+                const Text(
+                  'Github Finder',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+                const Text('Insira o usuário que deseja encontrar',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                    )),
+                Container(
+                  height: 30,
+                ),
+                const TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1.0)),
+                      hintText: 'Username',
+                      labelText: 'Username',
+                      labelStyle: TextStyle(color: Colors.black)),
+                ),
+                Container(
+                  height: 30,
+                ),
+                SizedBox(
+                    width: double.maxFinite,
+                    child: ElevatedButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                            padding: WidgetStatePropertyAll<EdgeInsetsGeometry>(
+                                EdgeInsets.fromLTRB(40, 10, 40, 10)),
+                            backgroundColor:
+                                WidgetStatePropertyAll<Color>(Colors.black)),
+                        child: const Text(
+                          'Buscar',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        )))
+              ]),
+            )
+          ],
+        ));
   }
 }
