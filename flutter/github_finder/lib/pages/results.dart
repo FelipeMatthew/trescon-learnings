6ceb4b181e4c22154d60a9bb6e1d1c4aecca06ff
felipematthew <felipematthewnascimento@gmail.com>
2024-08-10 23:07:57 -0300

    creating http get request

--- flutter/github_finder/lib/pages/results.dart
+++ flutter/github_finder/lib/pages/results.dart
@@ -1,7 +1,25 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:github_finder/models/user.dart';
+import 'package:http/http.dart' as http;
 
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key});
+
+  // HTTP REQUEST! SZ
+  Future<User> fetchUser() async {
+    var url = Uri.https('api.github.com', '/users/felipematthew');
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      // Converter o body string para map {}
+      final jsonBody = json.decode(response.body);
+      return User.fromJson(jsonBody);
+    } else {
+      throw Exception('Something got wrong');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
